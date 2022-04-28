74abb018a4a3856e67c2ff83bbb93f936ab76dc8
0400-Rashmi <daha6801@stthomas.edu>
2022-04-28 09:48:29 -0500

    added all items price and did cleanup with it to create and usable dataframe. Merged with the gas price dataframe

--- Inflation_Data.py
+++ Inflation_Data.py
@@ -4,7 +4,7 @@ pip install https://pypi.python.org/packages/source/P/PrettyTable/prettytable-0.
 # COMMAND ----------
 
 #dbutils.fs.help("put")
-dbutils.fs.rm("dbfs:/FileStore/my-stuff/APU0000747141.txt")
+dbutils.fs.rm("dbfs:/FileStore/my-stuff/CUSR0000SA05.txt")
 
 # COMMAND ----------
 
@@ -13,12 +13,17 @@ import requests
 import json
 import prettytable
 import os
+
+# COMMAND ----------
+
+# Read the Gasoline data based on the series id using the data tool API provided by US department of Labor. More information on the series ids https://download.bls.gov/pub/time.series/ap/ap.series
+# APU000074714  - Gasoline, unleaded regular, per gallon/3.785 liters in U.S. city average, average price, not seasonally adjusted		1976	M01	2022	M03  https://download.bls.gov/pub/time.series/ap/ap.data.2.Gasoline
+
 headers = {'Content-type': 'application/json'}
 auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
-# APU000074714  - Gasoline, unleaded regular, per gallon/3.785 liters in U.S. city average, average price, not seasonally adjusted		1976	M01	2022	M03  https://download.bls.gov/pub/time.series/ap/ap.data.2.Gasoline
 # CUSR0000SA0      	0000	SA0	S	R	S	1982-84=100	All items in U.S. city average, all urban consumers, seasonally adjusted		1947	M01	2022	M03  https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems
 startyeardate = 1976
-endyeardate = 2022
+endyeardate = 1986
 master_string = ""
 for i in range(0, 5):
     i = i+1
@@ -33,8 +38,6 @@ for i in range(0, 5):
         json_data = json.loads(p.text)
         startyeardate = startyeardate + 10;
         endyeardate = endyeardate + 10;
-        print(startyeardate)
-        print(endyeardate)
         
         #print(os.getcwd())
         for series in json_data['Results']['series']:
@@ -56,7 +59,53 @@ for i in range(0, 5):
             master_string = master_string + x.get_string()
             print(i)
             output.close()
-dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True)
+dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the gas price into a text file with the series id in its name
+
+# COMMAND ----------
+
+# Read the Gasoline data based on the series id using the data tool API provided by US department of Labor. More information on the series ids https://download.bls.gov/pub/time.series/cu/cu.series
+# CUSR0000SA0      	0000	SA0	S	R	S	1982-84=100	All items in U.S. city average, all urban consumers, seasonally adjusted		1947	M01	2022	M03  https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems
+
+headers = {'Content-type': 'application/json'}
+auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
+startyeardate = 1947
+endyeardate = 1957
+master_string = ""
+for i in range(0, 8):
+    i = i+1
+    print(i)
+    print(startyeardate)
+    print(endyeardate)
+    if startyeardate >= 1976 or endyeardate <= 2022:
+        data = json.dumps({"seriesid": ['CUSR0000SA0', 'CUSR0000SA0'],"startyear":str(startyeardate), "endyear":str(endyeardate)})
+        print(data)
+        p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers, auth = auth)
+        print(p.text)
+        json_data = json.loads(p.text)
+        startyeardate = startyeardate + 10;
+        endyeardate = endyeardate + 10;
+        
+        #print(os.getcwd())
+        for series in json_data['Results']['series']:
+            x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+            seriesId = series['seriesID']
+            for item in series['data']:
+                year = item['year']
+                period = item['period']
+                value = item['value']
+                footnotes=""
+                for footnote in item['footnotes']:
+                    if footnote:
+                        footnotes = footnotes + footnote['text'] + ','
+                if 'M01' <= period <= 'M12':
+                    x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+            output = open(seriesId + '.txt','w')
+            #print(seriesId)
+            output.write (x.get_string())
+            master_string = master_string + x.get_string()
+            print(i)
+            output.close()
+dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the all items price into a text file with the series id in its name
 
 # COMMAND ----------
 
@@ -65,16 +114,28 @@ display(dbutils.fs.ls("dbfs:/FileStore/my-stuff"))
 
 # COMMAND ----------
 
+#Read the gas price from the text file now
 gas_price = spark.read.text(
   'dbfs:/FileStore/my-stuff/APU0000747145.txt'
 )
 #gas_price.printSchema()
-display(gas_price)
+#display(gas_price)
+
+#Read the all items price from the text file now
+all_items_price = spark.read.text(
+  'dbfs:/FileStore/my-stuff/CUSR0000SA08.txt'
+)
+#all_items_price.printSchema()
+display(all_items_price)
 
 # COMMAND ----------
 
 from pyspark.sql.functions import *
 from pyspark.sql.functions import regexp_replace
+
+# COMMAND ----------
+
+#Format the gas price data by removing nulls, spliting and merging the columns to make a usable dataframe
 
 gas_price_chars_removed = gas_price.select(gas_price.value, regexp_replace(gas_price.value, r'\W+', ' ').alias("split_value")) # remove special characters from each row and replace with space
 gas_price_columns_split = gas_price_chars_removed.withColumn("seriesId", split(gas_price_chars_removed.split_value, " ").getItem(1)).withColumn("year", split(gas_price_chars_removed.split_value, " ").getItem(2)).withColumn("month", split(gas_price_chars_removed.split_value, " ").getItem(3)).withColumn("whole_value", split(gas_price_chars_removed.split_value, " ").getItem(4)).withColumn("decimal_value", split(gas_price_chars_removed.split_value, " ").getItem(5)) #split everything into it's own column
@@ -90,20 +151,63 @@ df_gas_price_null_value_removed_new = spark.createDataFrame(df_gas_price_null_va
 #df_gas_price_null_value_removed_new.show()
 
 df_gas_price_null_value_removed_new_combine_values = df_gas_price_null_value_removed_new.withColumn("actual_value", concat_ws('.', df_gas_price_null_value_removed_new.whole_value, df_gas_price_null_value_removed_new.decimal_value)) # merge the whole_value and decimal_value to form the original price
-df_gas_price_null_value_removed_new_combine_values.show()
+#df_gas_price_null_value_removed_new_combine_values.show()
+
+
+# COMMAND ----------
+
+#Format the all items price data by removing nulls, spliting and merging the columns to make a usable dataframe
+
+all_items_price_chars_removed = all_items_price.select(all_items_price.value, regexp_replace(all_items_price.value, r'\W+', ' ').alias("split_value")) # remove special characters from each row and replace with space
+all_items_price_columns_split = all_items_price_chars_removed.withColumn("seriesId", split(all_items_price_chars_removed.split_value, " ").getItem(1)).withColumn("year", split(all_items_price_chars_removed.split_value, " ").getItem(2)).withColumn("month", split(all_items_price_chars_removed.split_value, " ").getItem(3)).withColumn("whole_value", split(all_items_price_chars_removed.split_value, " ").getItem(4)).withColumn("decimal_value", split(all_items_price_chars_removed.split_value, " ").getItem(5)) #split everything into it's own column
+#display(all_items_price_columns_split.take(100))
+
+all_items_price_columns_split_updated_cols = all_items_price_columns_split.drop(all_items_price_columns_split.value) #drop the original column(value column) from the dataframe
+all_items_price_columns_split_updated_cols2 = all_items_price_columns_split_updated_cols.drop(all_items_price_columns_split_updated_cols.split_value) # drop split_value column from the dataframe
+#display(all_items_price_columns_split_updated_cols2.take(10))
+
+df_all_items_price_null_value_removed = all_items_price_columns_split_updated_cols2.na.drop() #drop any column that doesn't have any values
+#df_all_items_price_null_value_removed.show()
+df_all_items_price_null_value_removed_new = spark.createDataFrame(df_all_items_price_null_value_removed.tail(df_all_items_price_null_value_removed.count()-1), df_all_items_price_null_value_removed.schema) #remove the first column of the dataframe because it contains strings
+#df_all_items_price_null_value_removed_new.show()
+
+df_all_items_price_null_value_removed_new_combine_values = df_all_items_price_null_value_removed_new.withColumn("actual_value", concat_ws('.', df_all_items_price_null_value_removed_new.whole_value, df_all_items_price_null_value_removed_new.decimal_value)) # merge the whole_value and decimal_value to form the original price
+df_all_items_price_null_value_removed_new_combine_values.show()
 
 
 # COMMAND ----------
 
 import pyspark.sql.functions as f
+
+# COMMAND ----------
+
+# Calulate the average of gas price for each year by grouping by year and aggregating by it's actual_value
+
 df_gas_price_workable = df_gas_price_null_value_removed_new_combine_values.select(df_gas_price_null_value_removed_new_combine_values.year, df_gas_price_null_value_removed_new_combine_values.actual_value.cast('float')) #seclect only the year and the price of gasoline
 #display(df_gas_price_workable.take(5))
 
-grouped_aggregated = df_gas_price_workable.groupBy("year").agg(avg("actual_value").alias("gas price average")) #group by year, and take avg values of all the years
-grouped_aggregated_sorted = grouped_aggregated.orderBy(col("year")) # sort by year
-display(grouped_aggregated_sorted)                                                               
+gas_price_grouped_aggregated = df_gas_price_workable.groupBy("year").agg(avg("actual_value").alias("gas price average")) #group by year, and take avg values of all the years
+gas_price_grouped_aggregated_sorted = gas_price_grouped_aggregated.orderBy(col("year")) # sort by year
+display(gas_price_grouped_aggregated_sorted)                                                               
 
 
+
+# COMMAND ----------
+
+# Calulate the average of all items price for each year by grouping by year and aggregating by it's actual_value
+
+df_all_items_price_workable = df_all_items_price_null_value_removed_new_combine_values.select(df_all_items_price_null_value_removed_new_combine_values.year, df_all_items_price_null_value_removed_new_combine_values.actual_value.cast('float')) #seclect only the year and the price of gasoline
+#display(df_all_items_price_workable.take(5))
+
+all_tems_grouped_aggregated = df_all_items_price_workable.groupBy("year").agg(avg("actual_value").alias("all items price average")) #group by year, and take avg values of all the years
+all_tems_grouped_aggregated_sorted = all_tems_grouped_aggregated.orderBy(col("year")) # sort by year
+display(all_tems_grouped_aggregated_sorted)        
+
+# COMMAND ----------
+
+# Do outer Join to the two data sets for gas price and all items price by year, 
+gas_price_grouped_aggregated_sorted.join(all_tems_grouped_aggregated_sorted,gas_price_grouped_aggregated_sorted.year ==  all_tems_grouped_aggregated_sorted.year,"inner") \
+    .show(truncate=False)
 
 # COMMAND ----------
 
