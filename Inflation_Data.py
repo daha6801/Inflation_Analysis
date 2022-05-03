9b3eb7e06c531d5b70a9c1e128dc4b5264403505
0400-Rashmi <daha6801@stthomas.edu>
2022-05-03 16:41:03 -0500

    Added analysis for finding highest inflation and lowest inflation rate for gas price and all items price

--- Inflation_Data.py
+++ Inflation_Data.py
@@ -1,5 +1,7 @@
 # Databricks notebook source
-pip install https://pypi.python.org/packages/source/P/PrettyTable/prettytable-0.7.2.tar.bz2
+# install libraries
+%pip install https://pypi.python.org/packages/source/P/PrettyTable/prettytable-0.7.2.tar.bz2
+%pip install yfinance
 
 # COMMAND ----------
 
@@ -19,93 +21,93 @@ import os
 # Source the Gasoline data based on the series id using the data tool API provided by US department of Labor. More information on the series ids https://download.bls.gov/pub/time.series/ap/ap.series
 # APU000074714  - Gasoline, unleaded regular, per gallon/3.785 liters in U.S. city average, average price, not seasonally adjusted		1976	M01	2022	M03  https://download.bls.gov/pub/time.series/ap/ap.data.2.Gasoline
 
-headers = {'Content-type': 'application/json'}
-auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
-# CUSR0000SA0      	0000	SA0	S	R	S	1982-84=100	All items in U.S. city average, all urban consumers, seasonally adjusted		1947	M01	2022	M03  https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems
-startyeardate = 1976
-endyeardate = 1986
-master_string = ""
-for i in range(0, 5):
-    i = i+1
-    print(i)
-    print(startyeardate)
-    print(endyeardate)
-    if startyeardate >= 1976 or endyeardate <= 2022:
-        data = json.dumps({"seriesid": ['APU000074714', 'APU000074714'],"startyear":str(startyeardate), "endyear":str(endyeardate)})
-        print(data)
-        p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers, auth = auth)
-        print(p.text)
-        json_data = json.loads(p.text)
-        startyeardate = startyeardate + 10;
-        endyeardate = endyeardate + 10;
-        
-        #print(os.getcwd())
-        for series in json_data['Results']['series']:
-            x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
-            seriesId = series['seriesID']
-            for item in series['data']:
-                year = item['year']
-                period = item['period']
-                value = item['value']
-                footnotes=""
-                for footnote in item['footnotes']:
-                    if footnote:
-                        footnotes = footnotes + footnote['text'] + ','
-                if 'M01' <= period <= 'M12':
-                    x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-            output = open(seriesId + '.txt','w')
-            #print(seriesId)
-            output.write (x.get_string())
-            master_string = master_string + x.get_string()
-            print(i)
-            output.close()
-dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the gas price into a text file with the series id in its name
+#headers = {'Content-type': 'application/json'}
+#auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
+## CUSR0000SA0      	0000	SA0	S	R	S	1982-84=100	All items in U.S. city average, all urban consumers, seasonally adjusted		1947	M01	2022	M03  https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems
+#startyeardate = 1976
+#endyeardate = 1986
+#master_string = ""
+#for i in range(0, 5):
+#    i = i+1
+#    print(i)
+#    print(startyeardate)
+#    print(endyeardate)
+#    if startyeardate >= 1976 or endyeardate <= 2022:
+#        data = json.dumps({"seriesid": ['APU000074714', 'APU000074714'],"startyear":str(startyeardate), "endyear":str(endyeardate)})
+#        print(data)
+#        p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers, auth = auth)
+#        print(p.text)
+#        json_data = json.loads(p.text)
+#        startyeardate = startyeardate + 10;
+#        endyeardate = endyeardate + 10;
+#        
+#        #print(os.getcwd())
+#        for series in json_data['Results']['series']:
+#            x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+#            seriesId = series['seriesID']
+#            for item in series['data']:
+#                year = item['year']
+#                period = item['period']
+#                value = item['value']
+#                footnotes=""
+#                for footnote in item['footnotes']:
+#                    if footnote:
+#                        footnotes = footnotes + footnote['text'] + ','
+#                if 'M01' <= period <= 'M12':
+#                    x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+#            output = open(seriesId + '.txt','w')
+#            #print(seriesId)
+#            output.write (x.get_string())
+#            master_string = master_string + x.get_string()
+#            print(i)
+#            output.close()
+#dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the gas price into a text file with the series id in its name
 
 # COMMAND ----------
 
 # Source the All Items data based on the series id using the data tool API provided by US department of Labor. More information on the series ids https://download.bls.gov/pub/time.series/cu/cu.series
 # CUSR0000SA0      	0000	SA0	S	R	S	1982-84=100	All items in U.S. city average, all urban consumers, seasonally adjusted		1947	M01	2022	M03  https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems
 
-headers = {'Content-type': 'application/json'}
-auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
-startyeardate = 1947
-endyeardate = 1957
-master_string = ""
-for i in range(0, 8):
-    i = i+1
-    print(i)
-    print(startyeardate)
-    print(endyeardate)
-    if startyeardate >= 1947 or endyeardate <= 2022:
-        data = json.dumps({"seriesid": ['CUSR0000SA0', 'CUSR0000SA0'],"startyear":str(startyeardate), "endyear":str(endyeardate)})
-        print(data)
-        p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers, auth = auth)
-        print(p.text)
-        json_data = json.loads(p.text)
-        startyeardate = startyeardate + 10;
-        endyeardate = endyeardate + 10;
-        
-        #print(os.getcwd())
-        for series in json_data['Results']['series']:
-            x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
-            seriesId = series['seriesID']
-            for item in series['data']:
-                year = item['year']
-                period = item['period']
-                value = item['value']
-                footnotes=""
-                for footnote in item['footnotes']:
-                    if footnote:
-                        footnotes = footnotes + footnote['text'] + ','
-                if 'M01' <= period <= 'M12':
-                    x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-            output = open(seriesId + '.txt','w')
-            #print(seriesId)
-            output.write (x.get_string())
-            master_string = master_string + x.get_string()
-            print(i)
-            output.close()
-dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the all items price into a text file with the series id in its name
+#headers = {'Content-type': 'application/json'}
+#auth = HTTPBasicAuth('apikey','bf323d2b0dff40a79cf1df469caec067')
+#startyeardate = 1947
+#endyeardate = 1957
+#master_string = ""
+#for i in range(0, 8):
+#    i = i+1
+#    print(i)
+#    print(startyeardate)
+#    print(endyeardate)
+#    if startyeardate >= 1947 or endyeardate <= 2022:
+#        data = json.dumps({"seriesid": ['CUSR0000SA0', 'CUSR0000SA0'],"startyear":str(startyeardate), "endyear":str(endyeardate)})
+#        print(data)
+#        p = requests.post('https://api.bls.gov/publicAPI/v1/timeseries/data/', data=data, headers=headers, auth = auth)
+#        print(p.text)
+#        json_data = json.loads(p.text)
+#        startyeardate = startyeardate + 10;
+#        endyeardate = endyeardate + 10;
+#        
+#        #print(os.getcwd())
+#        for series in json_data['Results']['series']:
+#            x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+#            seriesId = series['seriesID']
+#            for item in series['data']:
+#                year = item['year']
+#                period = item['period']
+#                value = item['value']
+#                footnotes=""
+#                for footnote in item['footnotes']:
+#                    if footnote:
+#                        footnotes = footnotes + footnote['text'] + ','
+#                if 'M01' <= period <= 'M12':
+#                    x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+#            output = open(seriesId + '.txt','w')
+#            #print(seriesId)
+#            output.write (x.get_string())
+#            master_string = master_string + x.get_string()
+#            print(i)
+#            output.close()
+#dbutils.fs.put("dbfs:/FileStore/my-stuff/" + seriesId + str(i) + '.txt', master_string, overwrite=True) #save the all items price into a text file with the series id in its name
 
 # COMMAND ----------
 
@@ -284,12 +286,125 @@ display(all_tems_grouped_aggregated_sorted)
 # Calculate the average of all weekly wages for each year by grouping by year and aggregating by the weekly wage
 
 import pyspark.sql.functions as f
-df_wages_workable = df_wages_null_value_removed_new_combine_values.select(df_wages_null_value_removed_new_combine_values.year, df_wages_null_value_removed_new_combine_values.value1.cast('float'))
+df_wages_workable = df_wages_null_value_removed_new.select(df_wages_null_value_removed_new.year, df_wages_null_value_removed_new.value1.cast('float'))
 #display(df_gas_price_workable.take(5))
 
 grouped_aggregated = df_wages_workable.groupBy("year").agg(avg("value1").alias("wages average")) #group by year, and take avg values of all the years
 grouped_aggregated_sorted = grouped_aggregated.orderBy(col("year"))
 display(grouped_aggregated_sorted)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+# Import libraries
+import yfinance as yf
+import pandas as pd
+
+from datetime import date, datetime
+
+# Get BTC data from yahoo finance
+btc = yf.Ticker("BTC-USD")
+
+# Get max historical market data in 3 month intervals
+btc_price_data = btc.history(period="max", interval="3mo")
+
+# Get the closing data from history
+btc_price_closing = btc_price_data.Close
+#display(btc_price_closing)
+
+# Creating a list of quarterly dates
+# Data is collect quarterly (MM-dd) 
+
+# variables
+quarterly = ['-03-01', '-06-01', '-09-01', '-12-01']
+dates = []
+starting_year = 2014
+year_count = starting_year
+ending_date = date.today() # - 90
+index = 2
+run_loop = True
+
+# Loop that creates quartely dates
+while run_loop:
+    # Set a quarterly date
+    newDate = str(year_count)+quarterly[index]
+    datetimeFormat = datetime.strptime(newDate, "%Y-%m-%d").date()
+    
+    # Compare the todays date to quarterly date to stop loop
+    date1 = datetime(ending_date.year, ending_date.month, ending_date.day)
+    date2 = datetime(datetimeFormat.year, datetimeFormat.month, datetimeFormat.day)
+    
+    if (date1 < date2) == True:
+        run_loop = False
+    else:
+        dates.append(newDate)
+        
+    index+=1  
+    if index == 4:
+        index = 0
+        year_count+=1
+    
+
+#print(dates)    
+#print(str(date.today()))
+
+#len(dates)
+
+# Putting price to list
+price = []
+
+for i in btc_price_closing:
+    price.append(i)
+    
+#print(price)    
+
+# Length price list
+#len(price)
+
+# Creating PD dataframe
+price2 = price[:]
+
+try:
+    # try statement to keep length of dates equal to the length of prices
+    if len(dates) != len(price): # The price list collected a date that is no a quarterly date price
+        price2.pop()
+    else:
+        pass
+except ValueError:
+    print("Price list not the same length as dates list.")
+
+#len(price2) # Should be equal to the length of dates list
+
+
+# PD dataframe
+prices = {"Date": dates, "BTC Price": price2}
+#print(prices)
+btc_price = pd.DataFrame(prices)
+
+# Convert pandas DF to spark DF
+btc_rdd = spark.createDataFrame(btc_price)
+#btc_rdd.show()
+
+# Show Schema
+#btc_rdd.printSchema()
+
+# Show data
+#display(btc_rdd)
+
+# Show top 5 records
+#btc_rdd.take(5)
+
+#btc_df = btc_rdd.toDF()
+btc_rdd_columns_split = btc_rdd.withColumn("year", split(btc_rdd.Date, "-").getItem(0)).withColumn("month", split(btc_rdd.Date, "-").getItem(1)).withColumn("day", split(btc_rdd.Date, "-").getItem(2)) #split the date and time into separate columns
+btc_rdd_columns_split.printSchema()
+btc_rdd_columns_split.show()
+
+# COMMAND ----------
+
+btc_grouped_and_aggregated = btc_rdd_columns_split.groupBy("year").agg(avg("BTC Price").alias("BTC price average")) #group by year, and take avg values of all the years
+btc_grouped_and_aggregated_sorted = btc_grouped_and_aggregated.orderBy(col("year")) # sort by year
+display(btc_grouped_and_aggregated_sorted)  
 
 # COMMAND ----------
 
@@ -302,19 +417,67 @@ display(gas_all_items_avg_price_joined)
 # Join the dataframe with the weekly wage dataframe
 
 wage_gas_all_items_avg_price_joined = gas_all_items_avg_price_joined.join(grouped_aggregated_sorted,["year"])
-display(wage_gas_all_items_avg_price_joined)
+wage_gas_all_items_avg_price_joined_sorted = wage_gas_all_items_avg_price_joined.orderBy(col("year"))
+wage_gas_all_items_avg_price_joined_sorted_null_value_removed = wage_gas_all_items_avg_price_joined_sorted.na.drop()
+display(wage_gas_all_items_avg_price_joined_sorted_null_value_removed)
 
-#from pyspark.sql.functions import col
-#from pyspark.sql.window import Window
+
+# COMMAND ----------
+
+all_data_joined = gas_all_items_avg_price_joined.join(btc_grouped_and_aggregated_sorted,["year"])
+display(all_data_joined)
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+W = Window.orderBy('year')
+
+percent_change = gas_price_grouped_aggregated_sorted.withColumn('gas price average-1',f.lag(gas_price_grouped_aggregated_sorted['gas price average']).over(W))\
+  .withColumn('gas price(% change) year over year', f.round(((f.col('gas price average') - f.col('gas price average-1'))/f.col('gas price average-1')*100),2))
+#percent_change.show()
+gas_price_percent_change_only = percent_change.select(col("year"), col("gas price(% change) year over year"))
+display(gas_price_percent_change_only)
+
+# COMMAND ----------
+
+# When was the highest inlation rate for gas price between 1976 to 2022
+gas_price_percent_change_sorted_by_inflation = gas_price_percent_change_only.sort(col('gas price(% change) year over year').desc()).na.drop()
+#display(gas_price_percent_change_sorted_by_inflation)
+
+#select the highest inflation for gas price
+display(gas_price_percent_change_sorted_by_inflation.take(1))
+
+
+# COMMAND ----------
+
+# When was the lowest inlation rate for gas price between 1976 to 2022
+gas_price_percent_change_sorted_by_inflation = gas_price_percent_change_only.sort(col('gas price(% change) year over year').asc()).na.drop()
+#display(gas_price_percent_change_sorted_by_inflation)
+
+#select the highest inflation for gas price
+display(gas_price_percent_change_sorted_by_inflation.take(1))
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+W = Window.orderBy('year')
+
+all_items_percent_change = all_tems_grouped_aggregated_sorted.withColumn('all items price average-1',f.lag(all_tems_grouped_aggregated_sorted['all items price average']).over(W))\
+  .withColumn('all items price(% change) year over year', f.round(((f.col('all items price average') - f.col('all items price average-1'))/f.col('all items price average-1')*100),2))
+#percent_change.show()
+all_items_percent_change_only = all_items_percent_change.select(col("year"), col("all items price(% change) year over year"))
+display(all_items_percent_change_only)
+
+# COMMAND ----------
+
+# When was the highest inlation rate for gas price between 1976 to 2022
+all_items_percent_change_sorted_by_inflation = all_items_percent_change_only.sort(col('all items price(% change) year over year').desc()).na.drop()
+#display(gas_price_percent_change_sorted_by_inflation)
+
+#select the highest inflation for gas price
+display(all_items_percent_change_sorted_by_inflation.take(1))
 
 
 # COMMAND ----------
 
 
-#W = Window.orderBy('year')
-#
-#percent_change = grouped_aggregated_sorted.withColumn('gas price average-1',f.lag(grouped_aggregated_sorted['gas price average']).over(W))\
-#  .withColumn('var %', f.round((f.col('gas price average')/f.col('gas price average-1') -1)*100,2))
-##percent_change.show()
-#percent_change_only = percent_change.select(col("year"), col("var %"))
-#display(percent_change_only)
